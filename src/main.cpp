9c4ceadfb52096e3abd7dd40c6a63c03a8a46f73
Paul Schroeder <paulschroeder90@gmail.com>
2014-02-06 02:30:54 +0000

    Added basic 'size' classifier, parent pruning.
    No contour may have a child contour who is classified as text. When a text candidate is found, it pops
    up the tree, removing any parents from the candidate set.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <stack>
+#include <set>
 
 using namespace cv;
 
@@ -63,10 +64,10 @@ int main(int argc, char ** argv) {
   findContours(thresh_im, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
   std::stack<int> dfs_contour;
-  std::vector<int> text_candidate_contours;
+  std::set<int> text_candidate_contours;
 
   // Find first contour in first hierarchy
-  for (int i = 0; i < contours.size(); i++) {
+  for (size_t i = 0; i < contours.size(); i++) {
     if (hierarchy[i][1] == -1) {
       dfs_contour.push(i);
       std::cout << "Pushed " << i << std::endl;
@@ -77,21 +78,60 @@ int main(int argc, char ** argv) {
   // Traversal
   while (!dfs_contour.empty()) {
 
+    // Pop current contour
     int curr_contour = dfs_contour.top();
     dfs_contour.pop();
 
+    // Show it
+/*
     Mat cont_im = Mat::zeros(src.rows, src.cols, CV_8UC3);
     drawContours(cont_im, contours, curr_contour, Scalar(255,255,255), CV_FILLED, 8);
     imshow("Threshold Test", cont_im);
+*/
 
+    /*
+    *  Region filtering
+    */ 
+
+    // Size
+    Rect contour_bb = boundingRect(contours[curr_contour]);
+    std::cout <<  contour_bb.size().area() << std::endl;
+    if (contour_bb.size().area() > 0.5*src.rows*src.cols) {
+      std::cout << "rejecting region: too large" << std::endl;
+    } else if (contour_bb.size().area() < 100) {
+      std::cout << "rejecting region: too small" << std::endl;
+    } else {
+
+      // Add to candidates
+      text_candidate_contours.insert(curr_contour);
+
+      // Remove parents
+      int parent_contour = hierarchy[curr_contour][3];
+      while (parent_contour != -1) {
+        text_candidate_contours.erase(parent_contour);
+        parent_contour = hierarchy[parent_contour][3];
+      }
+      text_candidate_contours.insert(curr_contour);
+    }
+
+    // Push neighbor
     if (hierarchy[curr_contour][0] != -1) {
       dfs_contour.push(hierarchy[curr_contour][0]);
     }
 
+    // Push Children (This gets traversed first, if possible)
     if (hierarchy[curr_contour][2] != -1) {
       dfs_contour.push(hierarchy[curr_contour][2]);
     }
 
+
+  }
+
+  for (auto it = text_candidate_contours.begin(); it != text_candidate_contours.end(); ++it) {
+    // Show it
+    Mat cont_im = Mat::zeros(src.rows, src.cols, CV_8UC3);
+    drawContours(cont_im, contours, *it, Scalar(255,255,255), CV_FILLED, 8);
+    imshow("Threshold Test", cont_im);
     // Wait for escape keypress
     while (true) {
       int c;
@@ -101,5 +141,4 @@ int main(int argc, char ** argv) {
     } 
 
   }
-
 }
