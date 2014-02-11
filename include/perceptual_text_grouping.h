e8f898cd3d46f5ca33413b8a13dbf259682ce3e9
Philipp Cannons <philipp.cannons@gmail.com>
2014-02-11 01:06:36 +0000

    Added Delaunay triangulation algorithm which iterates through all text regions to retun text groups. Further narrowed down types for functions.

--- include/perceptual_text_grouping.h
+++ include/perceptual_text_grouping.h
@@ -8,13 +8,17 @@ using namespace cv;
 /* The text grouping stage takes the regions produced by the text segmentation step
    and makes compact groups of perceptually close or salient regions
 */
-void perceptual_text_grouping(Rect A, Rect B);
+Mat* perceptual_text_grouping(Rect* text_region_array);
 
 // Text saliency operator
-void text_saliency_operator(Rect A, Rect B);
+bool text_saliency_operator(Rect A, Rect B);
 
 // Scale-invariant saliency measures
-void relative_minimum_distance(Rect A, Rect B);
-void blob_dimension_ratio(Rect A, Rect B);
+double relative_minimum_distance(Rect A, Rect B);
+double blob_dimension_ratio(Rect A, Rect B);
+
+// Creation of planar graph using Delaunay triangulation
+CvSubdiv2D* construct_planar_graph(Rect* text_region_array);
+Point2f* calculate_region_center(Rect region);
 
 #endif
--- src/perceptual_text_grouping.cpp
+++ src/perceptual_text_grouping.cpp
@@ -3,23 +3,41 @@
 /* The text grouping stage takes the regions produced by the text segmentation step
    and makes compact groups of perceptually close or salient regions
 */
-void perceptual_text_grouping(Rect A, Rect B){
-
+Mat* perceptual_text_grouping(Mat* image, Rect* text_region_array){
+	// construct_planar_graph
+	// for edge e in graph
+	//   if !text_saliency_operator remove edge
+	// return connected nodes as text regions
+	return NULL;
 };
 
-void text_saliency_operator(Rect A, Rect B){
+bool text_saliency_operator(Rect A, Rect B){
 	//saliencey = normal_dist(blob_dimension_ratio()) 
 	//            * normal_dist(relative_minimum_distance())
+	return false;
 };
 
 // Takes two candidate text regions A and B as inputs
-void relative_minimum_distance(Rect A, Rect B){
+double relative_minimum_distance(Rect A, Rect B){
 	// return
 	// minimum_distance_btw_regions / (A_minimum_axes + B_maximum_axes)
+	return 0;
 };
 
-void blob_dimension_ratio(Rect A, Rect B){
+double blob_dimension_ratio(Rect A, Rect B){
 	// return 
 	// (A_minimum_zxes+A_maximum_axes)/(B_minimum_axes+B_maximum_axes)
+	return 0;
 };
+
+CvSubdiv2D* construct_planar_graph(Rect* text_region_array){
+	// for i in text_region_array
+	// 	add to CvSubdiv2D Delaunay graph(calculate_region_center(i))
+	return NULL;
+};
+
+Point2f* calculate_region_center(Rect region){
+	//return Point(region_x_max/2, region_y_max/2)
+	return NULL;
+}
 
