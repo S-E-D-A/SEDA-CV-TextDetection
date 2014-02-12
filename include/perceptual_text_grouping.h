df1ba17a1a99cce561e6152d737025452e32184f
Philipp Cannons <philipp.cannons@gmail.com>
2014-02-12 22:28:21 +0000

    Moved perceptual text grouping functions into their own namespace perceptual_text_grouping

--- include/perceptual_text_grouping.h
+++ include/perceptual_text_grouping.h
@@ -5,20 +5,22 @@
 
 using namespace cv;
 
-/* The text grouping stage takes the regions produced by the text segmentation step
-   and makes compact groups of perceptually close or salient regions
-*/
-Mat* perceptual_text_grouping(Rect* text_region_array);
+namespace perceptual_text_grouping {
+	/* The text grouping stage takes the regions produced by the text segmentation step
+	   and makes compact groups of perceptually close or salient regions
+	*/
+	Mat* perceptual_text_grouping(Rect* text_region_array);
 
-// Text saliency operator
-bool text_saliency_operator(Rect A, Rect B);
+	// Text saliency operator
+	bool text_saliency_operator(Rect A, Rect B);
 
-// Scale-invariant saliency measures
-double relative_minimum_distance(Rect A, Rect B);
-double blob_dimension_ratio(Rect A, Rect B);
+	// Scale-invariant saliency measures
+	double relative_minimum_distance(Rect A, Rect B);
+	double blob_dimension_ratio(Rect A, Rect B);
 
-// Creation of planar graph using Delaunay triangulation
-CvSubdiv2D* construct_planar_graph(Rect* text_region_array);
-Point2f* calculate_region_center(Rect region);
+	// Creation of planar graph using Delaunay triangulation
+	CvSubdiv2D* construct_planar_graph(Rect* text_region_array);
+	Point2f* calculate_region_center(Rect* text_region);
+}
 
 #endif
--- src/perceptual_text_grouping.cpp
+++ src/perceptual_text_grouping.cpp
@@ -1,42 +1,45 @@
 #include "perceptual_text_grouping.h"
 
-/* The text grouping stage takes the regions produced by the text segmentation step
-   and makes compact groups of perceptually close or salient regions
-*/
-Mat* perceptual_text_grouping(Mat* image, Rect* text_region_array){
-	// construct_planar_graph
-	// for edge e in graph
-	//   if !text_saliency_operator remove edge
-	// return connected nodes as text regions
-	return NULL;
-};
+namespace perceptual_text_grouping{
+	/* The text grouping stage takes the regions produced by the text segmentation step
+	   and makes compact groups of perceptually close or salient regions
+	*/
+	Mat* perceptual_text_grouping(Mat* image, Rect* text_region_array){
+		// construct_planar_graph
+		// for edge e in graph
+		//   if !text_saliency_operator remove edge
+		// return connected nodes as text regions
+		return NULL;
+	};
 
-bool text_saliency_operator(Rect A, Rect B){
-	//saliencey = normal_dist(blob_dimension_ratio()) 
-	//            * normal_dist(relative_minimum_distance())
-	return false;
-};
+	bool text_saliency_operator(Rect A, Rect B){
+		//saliencey = normal_dist(blob_dimension_ratio()) 
+		//            * normal_dist(relative_minimum_distance())
+		return false;
+	};
 
-// Takes two candidate text regions A and B as inputs
-double relative_minimum_distance(Rect A, Rect B){
-	// return
-	// minimum_distance_btw_regions / (A_minimum_axes + B_maximum_axes)
-	return 0;
-};
+	// Takes two candidate text regions A and B as inputs
+	double relative_minimum_distance(Rect A, Rect B){
+		// return
+		// minimum_distance_btw_regions / (A_minimum_axes + B_maximum_axes)
+		return 0;
+	};
 
-double blob_dimension_ratio(Rect A, Rect B){
-	// return 
-	// (A_minimum_zxes+A_maximum_axes)/(B_minimum_axes+B_maximum_axes)
-	return 0;
-};
+	double blob_dimension_ratio(Rect A, Rect B){
+		// return 
+		// (A_minimum_zxes+A_maximum_axes)/(B_minimum_axes+B_maximum_axes)
+		return 0;
+	};
 
-CvSubdiv2D* construct_planar_graph(Rect* text_region_array){
-	// for i in text_region_array
-	// 	add to CvSubdiv2D Delaunay graph(calculate_region_center(i))
-	return NULL;
-};
+	CvSubdiv2D* construct_planar_graph(Rect* text_region_array){
+		// for i in text_region_array
+		// 	add to CvSubdiv2D Delaunay graph(calculate_region_center(i))
+		return NULL;
+	};
 
-Point2f* calculate_region_center(Rect text_region){
-	return new Point2f(text_region.width/2, text_region.height/2);
+	Point2f* calculate_region_center(Rect* text_region){
+		Point2f* ret = new Point2f(text_region->width/2, text_region->height/2);
+		return ret;
+	}
 }
 
