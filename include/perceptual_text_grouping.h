0732b97edb2b25ec217c2aa27f5365deea58cc6f
Philipp Cannons <philipp.cannons@gmail.com>
2014-02-17 22:06:16 +0000

    Construct planar graph function implemented and tested in main.cpp but currently a test case cannot be created in gtest due to make test not working  properly resulting in undefined reference to opencv function

--- include/perceptual_text_grouping.h
+++ include/perceptual_text_grouping.h
@@ -2,27 +2,32 @@
 #define PERCEPTUAL_TEXT_GROUPING_H
 
 #include "opencv2/opencv.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <vector>
 
 using namespace cv;
 
 typedef Rect_<float> Rect4f;
 
 namespace perceptual_text_grouping {
-	/* The text grouping stage takes the regions produced by the text segmentation step
-	   and makes compact groups of perceptually close or salient regions
-	*/
-	Mat* perceptual_text_grouping(Rect4f* text_region_array);
+  /* The text grouping stage takes the regions produced by the text 
+     segmentation step and makes compact groups of perceptually 
+     close or salient regions
+  */
+  Mat* perceptual_text_grouping(Mat* image, vector<Rect4f*> text_region_array);
 
-	// Text saliency operator
-	bool text_saliency_operator(Rect4f* A, Rect4f* B);
+  // Text saliency operator
+  bool text_saliency_operator(Rect4f* A, Rect4f* B);
 
-	// Scale-invariant saliency measures
-	double relative_minimum_distance(Rect4f* A, Rect4f* B);
-	double blob_dimension_ratio(Rect4f* A, Rect4f* B);
+  // Scale-invariant saliency measures
+  double relative_minimum_distance(Rect4f* A, Rect4f* B);
+  double blob_dimension_ratio(Rect4f* A, Rect4f* B);
 
-	// Creation of planar graph using Delaunay triangulation
-	CvSubdiv2D* construct_planar_graph(Rect4f* text_region_array);
-	Point2f* calculate_region_center(Rect4f* text_region);
+  // Creation of planar graph using Delaunay triangulation
+  Subdiv2D* construct_planar_graph(vector<Rect4f*> text_region_array, 
+                            Rect image_size);
+  Point2f* rect_center_point(Rect4f* text_region);
 }
 
 #endif
--- src/perceptual_text_grouping.cpp
+++ src/perceptual_text_grouping.cpp
@@ -1,44 +1,54 @@
 #include "perceptual_text_grouping.h"
 
 namespace perceptual_text_grouping{
-	/* The text grouping stage takes the regions produced by the text segmentation step
-	   and makes compact groups of perceptually close or salient regions
-	*/
-	Mat* perceptual_text_grouping(Mat* image, Rect4f* text_region_array){
-		// construct_planar_graph
-		// for edge e in graph
-		//   if !text_saliency_operator remove edge
-		// return connected nodes as text regions
-		return NULL;
-	};
 
-	bool text_saliency_operator(Rect4f* A, Rect4f* B){
-		//saliencey = normal_dist(blob_dimension_ratio()) 
-		//            * normal_dist(relative_minimum_distance())
-		return false;
-	};
+  Mat* perceptual_text_grouping(Mat* image, vector<Rect4f*> text_region_array){
+    // construct_planar_graph
+    // for edge e in graph
+    //   if !text_saliency_operator remove edge
+    // return connected nodes as text regions
+    return NULL;
+  };
 
-	// Takes two candidate text regions A and B as inputs
-	double relative_minimum_distance(Rect4f* A, Rect4f* B){
-		// return
-		// minimum_distance_btw_regions / (A_minimum_axes + B_maximum_axes)
-		return 0;
-	};
+  bool text_saliency_operator(Rect4f* A, Rect4f* B){
+    //saliencey = normal_dist(blob_dimension_ratio()) 
+    //            * normal_dist(relative_minimum_distance())
+    return false;
+  };
 
-	double blob_dimension_ratio(Rect4f* A, Rect4f* B){
-		// return 
-		// (A_minimum_zxes+A_maximum_axes)/(B_minimum_axes+B_maximum_axes)
-		return 0;
-	};
+  // Takes two candidate text regions A and B as inputs
+  double relative_minimum_distance(Rect4f* A, Rect4f* B){
+    // return
+    // minimum_distance_btw_regions / (A_minimum_axes + B_maximum_axes)
+    return 0;
+  };
 
-	CvSubdiv2D* construct_planar_graph(Rect4f* text_region_array){
-		// for i in text_region_array
-		// 	add to CvSubdiv2D Delaunay graph(calculate_region_center(i))
-		return NULL;
-	};
+  double blob_dimension_ratio(Rect4f* A, Rect4f* B){
+    // return 
+    // (A_minimum_zxes+A_maximum_axes)/(B_minimum_axes+B_maximum_axes)
+    return 0;
+  };
 
-	Point2f* calculate_region_center(Rect4f* text_region){
-		return new Point2f(text_region->width/2, text_region->height/2);
-	}
+  Subdiv2D* construct_planar_graph(vector<Rect4f*> text_region_array, 
+    Rect image_size){
+
+    // Initialize graph with image size the maximum bounding box
+    Subdiv2D* planar_graph = new Subdiv2D(image_size);
+
+    MemStorage storage(cvCreateMemStorage(0));
+
+    // Insert each text region center into the graph
+    for(unsigned int i = 0; i < text_region_array.size(); i++){
+     Point2f* tmp_point = rect_center_point(text_region_array[i]);
+     planar_graph->insert(*tmp_point);
+    }
+
+    return planar_graph;
+  };
+
+  Point2f* rect_center_point(Rect4f* text_region){
+    // returns the center of a Rect4f as a Point2f
+    return new Point2f(text_region->width/2, text_region->height/2);
+  };
 }
 
