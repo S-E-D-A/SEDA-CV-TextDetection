61c1dd63e4dc504f5fa6703e72cd41f05d0174cb
ddetone <daniel.detone@gmail.com>
2014-04-12 12:57:09 -0400

    update model paths

--- src/main.cpp
+++ src/main.cpp
@@ -147,8 +147,8 @@ int main(int argc, char ** argv) {
         channels.push_back(255-channels[c]);
 
     // Create ERFilter objects with the 1st and 2nd stage default classifiers
-    Ptr<ERFilter> er_filter1 = createERFilterNM1(loadClassifierNM1("trained_classifierNM1.xml"),16,0.00015f,0.13f,0.2f,true,0.1f);
-    Ptr<ERFilter> er_filter2 = createERFilterNM2(loadClassifierNM2("trained_classifierNM2.xml"),0.5);
+    Ptr<ERFilter> er_filter1 = createERFilterNM1(loadClassifierNM1("models/trained_classifierNM1.xml"),16,0.00015f,0.13f,0.2f,true,0.1f);
+    Ptr<ERFilter> er_filter2 = createERFilterNM2(loadClassifierNM2("models/trained_classifierNM2.xml"),0.5);
 
     vector<vector<ERStat> > regions(channels.size());
     // Apply the default cascade classifier to each independent channel (could be done in parallel)
@@ -163,7 +163,7 @@ int main(int argc, char ** argv) {
     // Detect character groups
     cout << "Grouping extracted ERs ... ";
     vector<Rect> groups;
-    erGrouping(channels, regions, "trained_classifier_erGrouping.xml", 0.5, groups);
+    erGrouping(channels, regions, "models/trained_classifier_erGrouping.xml", 0.5, groups);
 
     // draw groups
     groups_draw(src, groups);
