1e76585f6d8990fa59976f363bdd0917fb6cc951
ddetone <daniel.detone@gmail.com>
2014-04-18 23:31:38 -0400

    Fix LMS estimation bug. Use LMS direction to plot through top-left corner

--- src/er_formwords.cpp
+++ src/er_formwords.cpp
@@ -72,10 +72,6 @@ bool compareERStat_sets(ERset s1, ERset s2)
 	it2 = s2.begin();
 	while ( it1 != s1.end() && it2 != s2.end() )
 	{
-		//const ERChar* er1_ptr = (*it1)Ptr<ERChar>();
-		//const ERChar* er1_ptr = (*it1)(ERChar*());
-		//const ERChar* er2_ptr = (*it2)Ptr<ERChar>();
-		//if ( er1_ptr != er2_ptr)
 		if ( !((*it1) == (*it2)) )
 			return false;
 		it1++;
@@ -86,111 +82,252 @@ bool compareERStat_sets(ERset s1, ERset s2)
 
 }
 
-//vector<vector<int> > GenerateCombinations(int n, int k)
+//vector<vector<int> > GenerateIndexPermutations()
 //{
+//	vector<int> myints = {0, 1, 2};
+//  sort (myints.begin(),myints.end());
 //
-//	vector<vector<int> > all_combs;
-//	vector<int> comb;
-//
-//	vector<bool> v(n);
-//  fill(v.begin() + n - k, v.end(), true);
-//
+//	vector< vector<int> > perms;
+//  //cout << "The 3! possible permutations with 3 elements:\n";
 //  do {
-//      for (int i = 0; i < n; ++i) {
-//          if (v[i]) {
-//							comb.push_back(i+1);
-//              //std::cout << (i+1) << " ";
-//          }
-//      }
-//      //std::cout << "\n";
-//			all_combs.push_back(comb);
-//			comb.clear();
-//  } while (std::next_permutation(v.begin(), v.end()));
+//    //cout << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+//		perms.push_back(myints);
+//  } while ( next_permutation(myints.begin(),myints.end()) );
 //
-//
-//	return all_combs;
+//  //cout << "After loop: " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+//	return perms;
 //}
-//
-//double median(Mat a, int N)
+
+//double calcError(vector<Point> pts, int idx, double a)
 //{
-//	Mat s;
-//	cv::sort(a,s, CV_SORT_EVERY_ROW);
+//	Mat Y = (Mat_<double>(3, 1) << pts[0].y, pts[1].y, pts[2].y);
+//	Mat X = (Mat_<double>(3, 1) << pts[0].x, pts[1].x, pts[2].x);
 //
-//	double med;
-//	if (N%2)
-//	{
-//		int middle1 = N/2;
-//		int middle2 = middle1 + 1;
-//		double med1 = s.at<double>(middle1);
-//		double med2 = s.at<double>(middle2);
-//		med = (med1 + med2)/2;
-//	}
-//	else
-//	{	
-//		int middle = N/2;
-//		med = s.at<double>(middle);
-//	}
+//	// idx is the index of the pts vector for which the potential line intersects
+//	double b = pts[idx].y - ( a * pts[idx].x ); 
+//	Mat B = (Mat_<double>(3, 1) << b, b, b);
+//	
+//	Scalar E = cv::sum(Y - ( a*X + B ) );
+//	double error = E[0]; 
+//	cout << "Error at idx " << idx << " is " << error << endl;
 //
-//	return med;
-//
+//	return error;
 //}
-//
-//Point LeastMedSquares(vector<Point> pts)
+
+//double calcError(vector<Point> pts, int idx_1, int idx_2, double a)
 //{
+//	Mat Y = (Mat_<double>(3, 1) << pts[0].y, pts[1].y, pts[2].y);
+//	Mat X = (Mat_<double>(3, 1) << pts[0].x, pts[1].x, pts[2].x);
 //
-//	int N = (int)pts.size();
+//	// idx_1 is the fixed index for one the the word lines
+//	// idx_2 is the potential index for one of the word lines
+//	double b = pts[intersect_idx].y - ( a * pts[intersect_idx].x ); 
+//	Mat B = (Mat_<double>(3, 1) << b, b, b);
+//	
+//	Scalar E = cv::sum(Y - ( a*X + B ) );
+//	double error = E[0]; 
+//	cout << "E0 " << E[0] << " E1 " << E[1] << endl;
 //
-//	Mat X = Mat::ones(N,2, CV_64F);		
-//	for (int n=0; n<N; n++)
-//		X.at<double>(n,1) = pts[n].x;
+//	return error;
+//}
+
+//pair<double,double> fitLines(vector<Point> pts, double a)
+//{
+//	// Only fit lines for triplets
+//	CV_Assert( pts.size() == 3 );
 //
-//	Mat y = Mat::zeros(N,1, CV_64F);
-//	for (int n=0; n<N; n++)
-//		y.at<double>(n,0) = pts[n].y;
-//
-//	vector<vector<int> > combs = GenerateCombinations(N, 3);
-//
-//	double a_best = 0;
-//	double b_best = 0;
-//	double d_min = 9999999;
-//
-//	for (int i=0; i<(int)combs.size(); i++)
+//	// Fit the first line
+//	double min_error = numeric_limits<double>::max();
+//	double min_idx_1 = 0;
+//	for (int i=0; i<(int)pts.size(); i++)
 //	{
-//		vector<int> idx;
-//		idx = combs[i];
-//
-//		double xi,xj,xk,yi,yj,yk;
-//		xi = pts[idx[0]].x;
-//		xj = pts[idx[1]].x;
-//		xk = pts[idx[2]].x;
-//		yi = pts[idx[0]].y;
-//		yj = pts[idx[1]].y;
-//		yk = pts[idx[2]].y;
-//
-//		double b = (yi - yk)/(xi - xk);
-//		double a = (yj + yk - b*(xj + xk) )/2;
-//		
-//		Mat w = Mat::zeros(2,1, CV_64F);		
-//		w.at<double>(0,0) = a;
-//		w.at<double>(1,0) = b;
-//		Mat r = cv::abs(y - X*w);
-//		double d = median(r,N);
-//
-//		if (d < d_min)
-//		{
-//			d_min = d;
-//			a_best = a;
-//			b_best = b;
-//		}
-//
+//			double error = calcError(pts, i, a);
+//			if (error < min_error)
+//			{
+//				min_error = error;
+//				min_idx_1 = i;
+//			}
 //	}
 //
-//	Point out;
-//	out.x = a_best;
-//	out.y = b_best;
+//	// Fit the second line
+//	double b = pts[min_idx_1].y - ( a * pts[min_idx_1].x );
+//	pair<double,double> line = make_pair(b, 0);
 //
-//	return out;
+//	return line;
 //}
+
+double median(vector<double> scores)
+{
+  size_t size = scores.size();
+  sort(scores.begin(), scores.end());
+
+  double median;
+  if (size  % 2 == 0)
+      median = (scores[size / 2 - 1] + scores[size / 2]) / 2;
+  else 
+      median = scores[size / 2];
+
+  return median;
+}
+
+double LeastMedSquaresDirection(vector<Point> pts)
+{
+
+	double a_best = 0;
+	double b_best = 0;
+	double d_min = numeric_limits<double>::max();
+
+	unsigned int combs[6][3] =
+	{
+		{2, 1, 0},
+		{2, 0, 1},
+		{1, 2, 0},
+		{1, 0, 2},
+		{0, 2, 1},
+		{0, 1, 2}
+	};
+
+	for (int i=0; i<6; i++)
+	{
+
+		// Choose new x and y combination
+		double xi,xj,xk,yi,yj,yk;
+		xi = (double)pts[combs[i][0]].x;
+		xj = (double)pts[combs[i][1]].x;
+		xk = (double)pts[combs[i][2]].x;
+		yi = (double)pts[combs[i][0]].y;
+		yj = (double)pts[combs[i][1]].y;
+		yk = (double)pts[combs[i][2]].y;
+
+		// Set up X and y matricies
+		Mat X = (Mat_<double>(3,2) << 1, xi, 1, xj, 1, xk);
+		Mat y = (Mat_<double>(3,1) << yi, yj, yk);
+
+		// Calculate slope and intercept for this permutation
+		double b = (yi - yk)/(xi - xk);
+		double a = (yj + yk - b*(xj + xk) )/2;
+		
+		// Calculate residuals
+		Mat w = Mat::zeros(2,1, CV_64F);		
+		w.at<double>(0,0) = a;
+		w.at<double>(1,0) = b;
+		Mat r = cv::abs(y - X*w);
+		cout << "R is " << r << endl;
+
+		// Find median
+		vector<double> findmymed(3);
+		findmymed[0] = r.at<double>(0,0);
+		findmymed[1] = r.at<double>(1,0);
+		findmymed[2] = r.at<double>(2,0);
+		double d = median(findmymed);
+	
+		// Update best median residual
+		if (d < d_min)
+		{
+			d_min = d;
+			a_best = a;
+			b_best = b;
+		}
+
+	}
+	cout << "a_best is " << a_best << endl;
+	cout << "b_best is " << b_best << endl;
+	return b_best;
+}
+
+//vector<pair<double,double> > estimateWordLines(ERset triplet)
+pair<double,double> estimateWordLines(ERset triplet)
+{
+	vector<Point> top_pts, bot_pts;
+	for (ERset::iterator it=triplet.begin(); it != triplet.end(); it++)
+	{
+		// Top-left corner
+		top_pts.push_back(it->rect.tl());
+
+		// Bottom-left corner
+		Point b = it->rect.br();
+		b.x = b.x + it->rect.width;
+		bot_pts.push_back(b);
+	}
+
+	//pair<double,double> best;
+	double slope = LeastMedSquaresDirection(top_pts);
+
+	//pair<double,double> top_intercepts  = fitLines(top_pts, slope);
+	// Returns: t1 and t2
+	
+
+
+	for (int i=0; i<top_pts.size(); i++)
+		cout << "top point " << i << " is (" << top_pts[i].x << ","<< top_pts[i].y <<")" << endl;
+
+	//cout << "intercept is " << top_intercepts.first << " " << top_intercepts.second << endl;
+
+	//lines.push_back( make_pair(a, top_intercepts.first) );
+	double b = top_pts[0].y - slope*top_pts[0].x;
+	//cout << "B IS " << b << endl;
+
+	vector<pair<double,double> > lines;
+	lines.push_back( make_pair(slope, b) );
+
+	return lines[0];
+}
+
+bool v3(ERset& triplet)
+{
+	CV_Assert( triplet.size() == 3);
+
+	// Draw the triplet
+	ERset::iterator it = triplet.begin();
+	int cols = (*it).im_ptr->cols;
+	int rows = (*it).im_ptr->rows;
+	Mat mask = Mat::zeros(rows+2, cols+2, CV_8UC1);
+	for ( ; it != triplet.end(); it++)
+	{
+		ERStat er = (*it);
+		int newmaskval = 255;
+		int flags = 4 + (newmaskval << 8) + FLOODFILL_FIXED_RANGE + FLOODFILL_MASK_ONLY;
+		Mat im = (*er.im_ptr);
+		floodFill(im,mask,Point(er.pixel%im.cols, er.pixel/im.cols), Scalar(255),0,Scalar(er.level),Scalar(0),flags);
+		rectangle(mask, er.rect.tl(), er.rect.br(), Scalar(255) );
+	}
+
+	// Estimate between 2 and 4 word lines
+	// pair< a (slope), b (intercept) >
+	//vector<pair<double,double> > lines = estimateWordLines(triplet);
+	pair<double,double> wordline = estimateWordLines(triplet);
+
+	// Get the word boundary points
+	it = triplet.begin();
+	Point TL = Point( it->rect.tl().x, it->rect.tl().y );
+	it = triplet.end();
+	it--;
+	Point TR = Point( it->rect.tl().x, it->rect.tl().y );
+	TR.x = TR.x + it->rect.width;
+	
+	//double slope = (double)(TR.y-TL.y) / (double)(TR.x-TL.x);
+	//cout << "slope is " << slope << endl;
+	double slope = wordline.first;
+	double b = wordline.second;
+	//cout << "calculated slope is " << 1/lines[0].first << endl;
+
+	//double bb = TL.y - slope*TL.x;
+
+	Point pp1 = Point(TL.x, (slope*TL.x)+b);
+	Point pp2 = Point(TR.x, (slope*TR.x)+b);
+
+	cout << "PPoint 1 is (" << pp1.x << "," << pp1.y << ")" << endl;
+	cout << "PPoint 2 is (" << pp2.x << "," << pp2.y << ")" << endl;
+
+	line(mask, pp1, pp2, Scalar(255), 2 );
+
+	cout << "drew line" << endl;
+	imshow("win", mask);
+	waitKey();
+	
+	return true;
+
+}
 
 void erShow(ERset &er_set, double delay)
 {
@@ -198,45 +335,36 @@ void erShow(ERset &er_set, double delay)
 	// Increment er_set iterator to first non-root ER, since the root ER may be a dummy ER
 	ERset::iterator it = er_set.begin();
 
+	int cols = (*it).im_ptr->cols;
+	int rows = (*it).im_ptr->rows;
+
 	// Grab the first ER and use its image pointer to initialize the size of a
 	// zeros image which will be used to display all the ERs in er_set
-	Mat mask = Mat::zeros((*it).im_ptr->rows+2, (*it).im_ptr->cols+2, CV_8UC1);
+	Mat mask = Mat::zeros(rows+2, cols+2, CV_8UC1);
 
-	//vector<Point> pts;
-	//Point leftmost, rightmost;
-	//leftmost.x = cols;
-	//rightmost.x = 0;
 	for ( ; it != er_set.end(); it++)
 	{
 		ERStat er = (*it);
-		//int c = (*it)->channel;
 		if (er.parent != NULL)
 		{
 			int newmaskval = 255;
 			int flags = 4 + (newmaskval << 8) + FLOODFILL_FIXED_RANGE + FLOODFILL_MASK_ONLY;
 			Mat im = (*er.im_ptr);
 			floodFill(im,mask,Point(er.pixel%im.cols, er.pixel/im.cols), Scalar(255),0,Scalar(er.level),Scalar(0),flags);
-			Point BR = er.rect.br();
-			BR.x = BR.x + 1; BR.y = BR.y + 1; //To account for mask size difference
-			//circle(masks[c], BR, 5, Scalar(255));
-			//pts.push_back(BR);
+			//Point BR = er.rect.br();
+			//BR.x = BR.x + 1; BR.y = BR.y + 1; //To account for mask size difference
+			//circle(mask, BR, 5, Scalar(255));
 
-			//if (BR.x < leftmost.x)
-			//	leftmost.x = BR.x;
-			//if (BR.x > rightmost.x)
-			//	rightmost.x = BR.x;
 		}
 	}
-	//Point lineParams = LeastMedSquares(pts);
-	//leftmost.y = lineParams.x + lineParams.y*leftmost.x;
-	//rightmost.y = lineParams.x + lineParams.y*rightmost.x;
-	//line(masks[0], leftmost, rightmost, Scalar(255), 2);
 
 	imshow("Regions", mask);
 	cvMoveWindow("Regions", 200, 50);
 	waitKey(delay);
 
 }	
+
+
 
 bool v1(const ERStat& er1, const ERStat& er2)
 {
@@ -479,6 +607,9 @@ void erFormWords(set<ERStat> &regions)
 					// eg. F
 					subset_1N.insert(subset_N);
 
+					if (subset_1N.size() == 3)
+						v3(subset_1N);
+
 					all_words_of_length.push_back(subset_1N);
 				}
 				subset_1N.clear();
@@ -496,12 +627,12 @@ void erFormWords(set<ERStat> &regions)
 	pruneSubwords(words);
 
 	// Show all ERs
-	for (int d=0; d<(int)words.size(); d++)
-	{
-		list<ERset>::iterator s;
-		for (s=words[d].begin(); s != words[d].end(); s++)
-			erShow((*s), 10);
-	}
+	//for (int d=0; d<(int)words.size(); d++)
+	//{
+	//	list<ERset>::iterator s;
+	//	for (s=words[d].begin(); s != words[d].end(); s++)
+	//		erShow((*s), 10);
+	//}
 
 }
 
